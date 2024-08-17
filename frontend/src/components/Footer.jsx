3ca754e0e833860796d087b4a0364bc89763455e
AnsHuLLranA <141855924+AnsHuLLranA@users.noreply.github.com>
2024-08-18 06:23:27 +1400

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">ANSHUL</div>
+          <div className="right">
+            <p>MOHALI 140413, PUNJAB</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By ANSHUL RANA</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By CodeWithANSHUL.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
